567c49d81823b2ece470a9a761951b74fd1a6db0
Srinivas H <srinivashulihyderseenu@gmail.com>
2026-07-23 00:01:41 +0530

    add route to display all listings

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Listing = require("./models/listing.js");
+const path=require("path");
 
 const MONG_URL = "mongodb://127.0.0.1:27017/wandernest";
+
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
 
 async function main() {
     await mongoose.connect(MONG_URL);
@@ -24,21 +29,35 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 });
 
+// Route to show all listings
+app.get("/listings", async (req, res) => {
 
+    // Fetch all data from DB
+    const allListings = await Listing.find({});
 
-app.get("/testListing", async(req,res)=>{
-     let sampleListing=new Listing({
-         title:"My New Villa",
-         description:"By the beach",
-        price:1200,
-         location:"Goa",
-         country:"India",
-     });
-
-     await sampleListing.save();
-     console.log("sample was saved");
-    res.send("Successfull testing");
+    // Render index.ejs and send data
+    res.render("listings/index", { allListings });
 });
+
+
+
+
+// app.get("/testListing", async(req,res)=>{
+//      let sampleListing=new Listing({
+//          title:"My New Villa",
+//          description:"By the beach",
+//         price:1200,
+//          location:"Goa",
+//          country:"India",
+//      });
+
+//      await sampleListing.save();
+//      console.log("sample was saved");
+//     res.send("Successfull testing");
+// });
+
+
+
 
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
