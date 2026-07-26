bab077192acdf0e48e7a09d8f74b7951fff92508
Srinivas H <srinivashulihyderseenu@gmail.com>
2026-07-26 23:14:12 +0530

    added delete route

--- app.js
+++ app.js
@@ -92,7 +92,12 @@ app.put("/listings/:id",async(req,res)=>{
 });
 
 
-
+//Delete Route
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
 
 
 // app.get("/testListing", async(req,res)=>{
