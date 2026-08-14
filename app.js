1b9053f6c051551908b9ebea71867448eccb2b81
Srinivas H <srinivashulihyderseenu@gmail.com>
2026-08-14 09:30:17 +0530

    Add Express error handling and error page

--- app.js
+++ app.js
@@ -129,7 +129,8 @@ app.all("/*splat", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     const { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).send(message);
+
+    res.status(statusCode).render("error.ejs", { err });
 });
 
 
