b95125e2591c78f4c5e3f2050137931e177e724d
Srinivas H <srinivashulihyderseenu@gmail.com>
2026-08-21 11:22:16 +0530

    validation of schema

--- README.md
+++
@@ -1,1 +0,0 @@
-Wandernest project 
--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema}=require("./schema.js");
 
 const MONG_URL = "mongodb://127.0.0.1:27017/wandernest";
 
@@ -32,10 +33,26 @@ main()
     });
 
 
+
+
+
 //Route 
 app.get("/",(req,res)=>{
     res.send("Hello World");
 });
+
+
+const validateListing = (req, res, next) =>{
+    let {error} = listingSchema.validate(req.body);
+    if (error) {
+        let msg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(400, msg);
+    }
+    else
+    {
+        next();
+    }
+};
 
 // Route to show all listings
 app.get("/listings", wrapAsync(async (req, res) => {
@@ -69,10 +86,8 @@ app.get("/listings/:id", wrapAsync(async(req,res)=>{
 
 //create route 
 
-app.post("/listings", wrapAsync(async (req, res, next) => {
-    if (!req.body.listing) {
-        throw new ExpressError(400, "Invalid Listing Data");
-    }
+app.post("/listings",validateListing, wrapAsync(async (req, res, next) => {
+  
     const newListing = new Listing(req.body.listing);
     await newListing.save();
 
@@ -89,10 +104,7 @@ app.get("/listings/:id/edit", wrapAsync(async(req,res)=>{
 
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async(req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"Invalid Listing Data");
-    }
+app.put("/listings/:id",validateListing, wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let listing=req.body.listing;
     await Listing.findByIdAndUpdate(id,listing);
@@ -101,7 +113,7 @@ app.put("/listings/:id", wrapAsync(async(req,res)=>{
 
 
 //Delete Route
-app.delete("/listings/:id", wrapAsync(async(req,res)=>{
+app.delete("/listings/:id", validateListing, wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
